e7284185a7caca7f707e9f6076556a2b34c4d693
Kevin Hall <omegak@live.com>
2021-02-04 05:59:14 -0500

    Created base profile page

--- index/src/App.js
+++ index/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import proImage from './assets/images/kh_picture.jpg';
+import logo from './logo.svg'
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "header_name">
+      Kevin Hall
     </div>
+  );
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button className = "left-side_button" onClick = {handleClick}>{text}</button>
+  );
+}
+
+const MainPageContent = () =>
+{
+  return (
+    <div className = "content-area">
+      <div>
+        <span>Under Construction...</span>
+        <img className = 'logo' src = {logo} alt="React" />
+      </div>
+    </div>
+  );
+}
+
+const SideBySideLayout = () =>
+{
+  return (
+    <div className = "side-by-side_layout">
+      <div className = "left-column">
+        <div>
+          <img src = {proImage} alt="Kevin Hall" />
+        </div>
+        <div>
+          <Button handleClick = {() => console.log("About Click")} text = "About" />
+          <Button handleClick = {() => console.log("Resume Click")} text = "Resume" />
+          <Button handleClick = {() => console.log("Contact Click")} text = "Contact" />
+        </div>
+      </div>
+      <div className = "right-column">
+        <Header />
+        <MainPageContent />
+      </div>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <SideBySideLayout />
   );
 }
 
