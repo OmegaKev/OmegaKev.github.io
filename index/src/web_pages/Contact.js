153e979d14890a6b1d0eb9f727466357337f568e
Kevin Hall <omegak@live.com>
2021-02-06 07:18:37 -0500

    Created basic email form in Contact

--- index/src/web_pages/Contact.js
+++ index/src/web_pages/Contact.js
@@ -1,8 +1,21 @@
+// Disables form submit
+// Will be used until php code is finished
+const disable_submit = (event) => {
+    event.preventDefault();
+    alert('E-mail not implemented.');
+}
+
 const Page_Contact = () =>
 {
     return (
         <div>
             <span>Contact Page</span>
+            <form className="email-form text-border" onSubmit={disable_submit}>
+                <span>Name:</span><input type="text"></input>
+                <span>Subject:</span><input type="text"></input>
+                <textarea className= "item-e"></textarea>
+                <input type="submit" value="Send" />
+            </form>
         </div>
       );
 }
