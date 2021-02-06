c4d1bc25278edf2a359f0350435db3732d5b745c
Kevin Hall <omegak@live.com>
2021-02-06 06:05:47 -0500

    Added some text to about me

--- index/src/web_pages/About.js
+++ index/src/web_pages/About.js
@@ -2,7 +2,12 @@ const Page_About = () =>
 {
     return (
         <div>
-            <span>About Page</span>
+            <span>About Me</span>
+            <span className="text-border">
+                <p>I have an excellent academic record at the master’s level, ability to debug software, fluent in a variety of programming languages such as C++, Java, C#, Python, JavaScript and front-end web design experience using HTML5, and CSS.</p>
+                <p>I have a strong understanding of functional designs and structures and I am experienced in supporting a team in an agile work environment as a Software Engineer utilizing version control systems such as Git, and Bitbucket.</p>
+                <p>I am committed to expanding my knowledge and experience with new technologies to advance my role in an innovative and forward-thinking organization.</p>
+            </span>
         </div>
       );
 }
