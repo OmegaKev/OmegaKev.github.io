9bcc8e00dd805b4fa416901151e111d9ca1e8933
Kevin Hall <omegak@live.com>
2021-02-16 04:48:34 -0500

    Adjusted Box size on creation

--- index/src/web_pages/Projects.js
+++ index/src/web_pages/Projects.js
@@ -2,8 +2,9 @@ import {useState, useEffect, useCallback, useRef} from 'react';
 import './projects.css';
 
 const RepoElement = ({repo}) => {
-    const [repoBody, setRepoBody] = useState({open: true, size: 100});
+    const [repoBody, setRepoBody] = useState({open: true, size: 'auto'});
     const animation_interval = useRef();
+    const max_height = useRef();
     
     // Hold a reference to the state of the element
     const state = useRef();
@@ -11,10 +12,10 @@ const RepoElement = ({repo}) => {
 
     const setSize = useCallback((value) => {
         // Keep new value within 0 - 100 range
-        let new_value = Math.max(0, Math.min(state.current.size + value, 100));
+        let new_value = Math.max(0, Math.min(state.current.size + value, max_height.current));
 
         setRepoBody(repoBody => ({open: repoBody.open, size: new_value}));
-    }, []);
+    }, [max_height]);
 
     const setOpenState = useCallback((state) => {
         setRepoBody(repoBody => ({open: state, size: repoBody.size}));
@@ -35,52 +36,57 @@ const RepoElement = ({repo}) => {
         setSize(speed);
 
         // Animation is done if it reaches 0 or 100
-        if(repoBody.size + speed  <= 0 || repoBody.size + speed >= 100)
+        if(repoBody.size + speed  <= 0 || repoBody.size + speed >= max_height.current)
             clearInterval(animation_interval.current);
         
 
-    }, [setSize]);
+    }, [setSize, max_height]);
 
-
+    // Set max height of list
     useEffect(() => {
-        // Animate closing/opening project boxes
-        if(animation_interval.current)clearInterval(animation_interval.current);
-        animation_interval.current = setInterval(alterRepoBody, 10);
+        max_height.current = document.getElementById(repo.id).clientHeight;
+        setRepoBody(repoBody => ({open: repoBody.open, size: max_height.current}));
+        console.log(max_height.current);
+    }, []);
+
+    // Animate closing/opening project boxes
+    useEffect(() => {
+        if(!isNaN(max_height.current))
+            animation_interval.current = setInterval(alterRepoBody, 10);
+        return () => {
+            if(animation_interval.current)clearInterval(animation_interval.current);
+        }
     }, [repoBody.open]);
 
     return (
-        <>
-            <div className="repo-name" onClick={toggleRepoBody} >
+        <li key ={repo.id} className="list">
+            <div className="repo-name" onClick={toggleRepoBody}>
                 <span>{repo.name}</span>
             </div>
-
-            <div className="repo-body" style={{'height': repoBody.size}}>
+            <div className="repo-body" id={repo.id} style={{height: repoBody.size}}>
                 <div className = "repo-body-l-child">
                     <span>Image</span>
                 </div>
                 <div className = "repo-body-r-child">
-                    <div><span><a href = {repo.html_url} target="_blank">{repo.html_url}</a></span><span>Run</span></div>
+                    <div><span><a href = {repo.html_url} target="_blank" rel="noreferrer">{repo.html_url}</a></span><span>Run</span></div>
                     <span>{repo.description}</span>
                 </div>
             </div>
-        </>
+        </li>
     );
 }
 
 // Repo List Component
 // Generate the github list of public projects
 const RepoList = ({repos}) => {
-    
-    if(!repos || repos.length === 0) return (<p>No Repos were loaded...</p>);
+// Check and make sure we have a valid repo list
+    if(!repos || repos.length === 0 || repos.message) return (<p> Encounted an Github API server error.</p>);
+
     return (
         <ul>
             <h2 className="list-head">Public Github Repository</h2>
             {repos.map((repo) => {
-                return(
-                    <li key ={repo.id} className="list">
-                        <RepoElement repo = {repo}/>
-                    </li>
-                );
+                return(<RepoElement repo = {repo} key={repo.id} />);
             })}
         </ul>
     );
